c729b2b0f6aa0d4ec63c5847a6fe82781c5438a4
Gustavo Prevelate <guprevelate2022@gmail.com>
2023-05-14 21:32:20 -0300

    Adicionando responsividade api.html 600px e 375px

--- js/router.js
+++ js/router.js
@@ -1,24 +1,49 @@
+// 'use strict'
+
+//   const routes = {
+//     '/superheroes'              :   '/pages/superheroes.html',
+//     '/api'                      :   '/pages/api.html',
+//     '/herois'                   :   '/pages/herois.html',
+// }
+
+// const route = async () => {
+//     window.event.preventDefault()
+//     window.history.pushState({},"", window.event.target.href)
+    
+//     const path = window.location.pathname
+//     const route = routes[path]
+
+//     const response = await fetch(route)
+//     const html = await response.text()
+
+//     document.getElementById('root').innerHTML = html
+
+//     console.log(html)
+// }
+
+// window.route = route
+
 'use strict'
 
-  const routes = {
-    '/superheroes'              :   '../pages/superheroes.html',
-    '/api'                      :   '../pages/api.html',
-    '/herois'                   :   '../pages/herois.html',
+const routes = {
+  '/superheroes'              :   '../pages/superheroes.html',
+  '/api'                      :   '../pages/api.html',
+  '/herois'                   :   '../pages/herois.html',
 }
 
 const route = async () => {
+     
     window.event.preventDefault()
-    window.history.pushState({},"", window.event.target.href)
-    
+    window.history.pushState({},"", window.event.currentTarget.href)
+        
     const path = window.location.pathname
     const route = routes[path]
-
+    
     const response = await fetch(route)
     const html = await response.text()
-
+    
     document.getElementById('root').innerHTML = html
-
-    console.log(html)
+    
 }
 
 window.route = route
